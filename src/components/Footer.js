2c468f02f9aa2e2fc50937b01625cee748f429e6
Siehan <memain.ye.sylvie@gmail.com>
2021-07-06 15:46:08 +0200

    update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,25 @@ import { Box } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Box
-      as="footer"
-      p={3}
-      bg="gray.800"
-      color="white"
-      textAlign="center"
-      borderRadius="md"
-      fontSize={18}
-      bgGradient="linear(to-r, blue.600, green.400)"
-    >
-      Made with <span className="text-danger">&hearts;</span>
-      <span className="text-success">&hearts;</span>
-      <span className="text-primary">&hearts;</span>
-      <span className="text-warning">&hearts;</span> by Jonathan, Nassim, Victor & Sylvie - The Robinet Token Red Team
+    <Box as="footer">
+      <Box
+        p={3}
+        textAlign="center"
+        alignItems="center"
+        borderRadius="md"
+        maxW={"100%"}
+        mx="auto"
+        fontSize={19}
+        bgGradient="linear(to-l, #3CAADD, #4FAA1B)"
+        _hover={{
+          bgGradient: "linear(to-r, blue.600, green.400)",
+        }}
+      >
+        Made with <span className="text-danger">&hearts;</span>
+        <span className="text-success">&hearts;</span>
+        <span className="text-primary">&hearts;</span>
+        <span className="text-warning">&hearts;</span> by Jonathan, Nassim, Victor & Sylvie - The Robinet Token Red Team
+      </Box>
     </Box>
   );
 };
