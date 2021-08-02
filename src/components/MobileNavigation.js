bfb187adec704731a3e3a53cda178268b0e89986
Siehan <memain.ye.sylvie@gmail.com>
2021-08-02 11:54:41 +0200

    update box hamburger

--- src/components/MobileNavigation.js
+++ src/components/MobileNavigation.js
@@ -8,7 +8,17 @@ const MobileNavigation = ({ children }) => {
   return (
     <>
       <Button as={IconButton} aria-label="Options" icon={<HamburgerIcon />} variant="outline" onClick={onToggle} />
-      <Box position="absolute" left="0" right="0" top="100%" shadow="xl" bg={bg}>
+      <Box
+        position="absolute"
+        left="0"
+        right="0"
+        top="100%"
+        shadow="xl"
+        bg={bg}
+        bgGradient="linear(to-r, green.400, blue.400)"
+        borderRadius="md"
+        opacity="0.7"
+      >
         <Fade in={isOpen} unmountOnExit={true}>
           <Box p="4">{children}</Box>
         </Fade>
